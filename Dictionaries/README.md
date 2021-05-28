3583a82a85ce49cd86f60045f5bb51ac7ed239a7
malladi2610 <subhashmalladi@yahoo.com>
2021-05-28 11:49:43 +0530

    Dictionaries_introduction

---
+++ Dictionaries/README.md
@@ -0,0 +1,11 @@
+#Dictionaries
+
+#They accept elements of different datatype - individual elements, tuples,lists,strings
+
+#Indices are of immutable datatypes(string,num,tuple)
+
+#Creating an empty Dictionary
+d = {} or a = dict()
+
+#Dict are mutable
+
---
+++ Dictionaries/introduction.py
@@ -0,0 +1,48 @@
+#Declaration of Dictionaries
+
+employee = {'name' : 'john', 'company' : 'abc', 'salary' : 100000,'age' : 20}
+#print(employee)
+
+q = dict(name = 'john', comp = 'abc')
+#print(q)
+
+#Here the list is converted to dictionaries(used when the key values are present)
+w = dict([['name','john'],['company','abc'],['sal',100000]])
+#print(w)
+
+#Here the tuple is converted to dictionaries
+w = dict((('name','john'),('company','abc'),('sal',100000)))
+#print(w)
+
+#Adding elements and changing to the dict
+q['name'] = 'ben'
+q['salary'] = 100000
+q['age'] = 30
+#print(q)
+
+#Displaying the elements
+
+"""
+#Printing the indices
+for i in q:
+    print(i)
+
+#printing the values
+
+for i in q:
+    print(q[i])   
+
+#printing both indices and values
+
+for i in q:
+    print(i,q[i])  
+"""
+    
+#Displays the list of keys
+ 
+print(list(q.keys()))
+
+print(list(q.values()))
+
+a = list(q.keys())
+print(a[0])
